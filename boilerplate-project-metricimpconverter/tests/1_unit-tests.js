9d0a4c5696267d28c50fda431a057806f539dddc
neeraj <neerajp4321@gmail.com>
2022-10-26 12:29:06 +0530

    Feat: complete unit tests for 1 project

--- boilerplate-project-metricimpconverter/tests/1_unit-tests.js
+++ boilerplate-project-metricimpconverter/tests/1_unit-tests.js
@@ -4,6 +4,84 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 
 let convertHandler = new ConvertHandler();
 
-suite('Unit Tests', function(){
+suite('Unit Tests', function () {
+
+  suite('input number tests', function () {
+
+    test('whole number input', function () {
+      assert.match(convertHandler.getNum('1gal'), /^[0-9]+$/)
+    })
+
+    test('decimal number input', function () {
+      assert.match(convertHandler.getNum('1.4gal'), /^\d*\.\d+$/)
+    })
+
+    test('fractional input', function () {
+      assert.match(convertHandler.getNum('1/4gal'), /^\d*\.\d+$/)
+    })
+
+    test('fraction input with decimal', function () {
+      assert.match(convertHandler.getNum('3.3/2gal'), /^\d*\.\d+$/)
+    })
+
+    test('double fraction error', function () {
+      assert.equal(convertHandler.getNum('4/4/4gal'), 'invalid number')
+    })
+
+    test('default input 1 when not provided', function () {
+      assert.isNotNaN(convertHandler.getNum('gal'))
+    })
+
+  })
+
+  suite('input unit tests', function () {
+
+    const { validInputs, unitConversion, spell } = require('../controllers/data.js')
+
+    const testUnit = 'L'
+
+    test('valid input unit', function () {
+      assert.include(validInputs, convertHandler.getReturnUnit(testUnit))
+    })
+
+    test('invalid input unit', function () {
+      assert.notInclude(validInputs, convertHandler.getReturnUnit('gale'), 'invalid unit')
+    })
+
+    test('correct return unit', function () {
+      assert.equal(convertHandler.getReturnUnit(testUnit), unitConversion[testUnit.toLowerCase()])
+    })
+
+    test('spelled out unit name', function () {
+      assert.equal(convertHandler.spellOutUnit(testUnit), spell[testUnit.toLowerCase()])
+    })
+
+  })
+
+  suite('conversion of value in different unit', function () {
+    test('gal to L', function () {
+      assert.equal(convertHandler.convert(1, 'gal'), 3.78541)
+    })
+
+    test('L to gal', function () {
+      assert.equal(convertHandler.convert(3.78541, 'L'), 1)
+    })
+
+    test('mi to km', function () {
+      assert.equal(convertHandler.convert(1, 'mi'), 1.60934)
+    })
+
+    test('km to mi', function () {
+      assert.equal(convertHandler.convert(1.60934, 'km'), 1)
+    })
+
+    test('lbs to kg', function () {
+      assert.equal(convertHandler.convert(1, 'lbs'), 0.45359)
+    })
+
+    test('kg to lbs', function () {
+      assert.equal(convertHandler.convert(0.45359, 'kg'), 1)
+    })
+  })
 
 });
--- boilerplate-project-metricimpconverter/tests/2_functional-tests.js
+++ boilerplate-project-metricimpconverter/tests/2_functional-tests.js
@@ -5,6 +5,51 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite('Functional Tests', function () {
+
+  test('10L get request to /api/convert', function () {
+    chai.request(server)
+      .get('/api/convert?input=10L')
+      .end(function (err, res) {
+        assert.equal(res.status, 200)
+        assert.equal(res.text, '{"initNum":10,"initUnit":\"L\","returnNum":2.64172,"returnUnit":\"gal\","string":\"10 liters converts to 2.64172 gallons\"}')
+      })
+  })
+
+  test('32g get /api/convert', function () {
+    chai.request(server)
+      .get('/api/convert?input=32g')
+      .end(function (err, res) {
+        assert.equal(res.status, 200)
+        assert.equal(res.text, 'invalid unit')
+      })
+  })
+
+  test('3/7.2/4kg get /api/convert', function () {
+    chai.request(server)
+      .get('/api/convert?input=3/7.2/4kg')
+      .end(function (err, res) {
+        assert.equal(res.status, 200)
+        assert.equal(res.text, 'invalid number')
+      })
+  })
+
+  test('3/7.2/4kilomegagram get /api/convert', function () {
+    chai.request(server)
+      .get('/api/convert?input=3/7.2/4kilomegagram')
+      .end(function (err, res) {
+        assert.equal(res.status, 200)
+        assert.equal(res.text, 'invalid number and unit')
+      })
+  })
+
+  test('kg get /api/convert', function () {
+    chai.request(server)
+      .get('/api/convert?input=kg')
+      .end(function (err, res) {
+        assert.equal(res.status, 200)
+        assert.equal(res.text, '{"initNum":1,"initUnit":\"kg\","returnNum":2.20462,"returnUnit":\"lbs\","string":\"1 kilograms converts to 2.20462 pounds\"}')
+      })
+  })
 
 });

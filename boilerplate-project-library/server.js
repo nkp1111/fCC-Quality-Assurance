15a18df2e928cef182613565bca6bc212c022bb6
neeraj <neerajp4321@gmail.com>
2022-10-28 11:36:36 +0530

    Feat: add mongoose and connected

--- boilerplate-project-library/server.js
+++ boilerplate-project-library/server.js
@@ -1,22 +1,33 @@
 'use strict';
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
 
 const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
+app.use(cors({ origin: '*' })); //USED FOR FCC TESTING PURPOSES ONLY!
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// to connect mongoose
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env['DB'])
+  .then(() => {
+    console.log('Mongoose connected')
+  })
+  .catch((err) => {
+    console.log('error', err)
+  })
 
 //Index page (static HTML)
 app.route('/')
@@ -28,10 +39,10 @@ app.route('/')
 fccTestingRoutes(app);
 
 //Routing for API 
-apiRoutes(app);  
-    
+apiRoutes(app);
+
 //404 Not Found Middleware
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
@@ -40,14 +51,14 @@ app.use(function(req, res, next) {
 //Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
     setTimeout(function () {
       try {
         runner.run();
-      } catch(e) {
-          console.log('Tests are not valid:');
-          console.error(e);
+      } catch (e) {
+        console.log('Tests are not valid:');
+        console.error(e);
       }
     }, 1500);
   }

eccaa382340ad168a7d34f6444918ea37629d5e4
neeraj <neerajp4321@gmail.com>
2022-10-31 10:03:36 +0530

    Feat: add routes to solve and check ans

--- boilerplate-project-sudoku-solver/controllers/sudoku-solver.js
+++ boilerplate-project-sudoku-solver/controllers/sudoku-solver.js
@@ -1,10 +1,31 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    let puzzlePieces = puzzleString.split('')
+
+    if (puzzlePieces.length !== 81) {
+      return 'Expected puzzle to be 81 characters long'
+    }
+    puzzlePieces = puzzlePieces.filter((ele) => {
+      if (/\d/.test(ele) || ele === '.') {
+        return ele
+      }
+    })
+
+    if (puzzlePieces.length !== 81) {
+      return 'Invalid characters in puzzle'
+    }
+    else {
+      return true
+    }
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
+    puzzleString.map(d => {
+      if (puzzleString.includes(d)) {
 
+      }
+    })
   }
 
   checkColPlacement(puzzleString, row, column, value) {
@@ -16,7 +37,7 @@ class SudokuSolver {
   }
 
   solve(puzzleString) {
-    
+
   }
 }
 
--- boilerplate-project-sudoku-solver/routes/api.js
+++ boilerplate-project-sudoku-solver/routes/api.js
@@ -3,16 +3,30 @@
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
 module.exports = function (app) {
-  
+
   let solver = new SudokuSolver();
 
   app.route('/api/check')
     .post((req, res) => {
+      const { puzzle, value, coordinate } = req.body
+      solver.validate(puzzle)
+      let column
+      puzzle.split('').map(d => {
 
+      })
+      res.send(req.body)
     });
-    
+
   app.route('/api/solve')
     .post((req, res) => {
-
+      const { puzzle, value, coordinate } = req.body
+      if (!puzzle) {
+        res.send({ error: 'Required field missing' })
+      }
+      let validation = solver.validate(puzzle)
+      if (validation !== true) {
+        res.send({ error: validation })
+      }
+      res.send(req.body)
     });
 };

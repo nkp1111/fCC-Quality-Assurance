f12676a2b9c58da09fe256816071445d6b285673
neeraj <neerajp4321@gmail.com>
2022-10-31 16:21:22 +0530

    Feat: add row, column and region from puzzleString

--- boilerplate-project-sudoku-solver/routes/api.js
+++ boilerplate-project-sudoku-solver/routes/api.js
@@ -31,7 +31,62 @@ module.exports = function (app) {
         res.send({ error: 'Invalid value' })
       }
 
-      res.send(req.body)
+      // coordinates
+      const checkRow = { 'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9 }
+      const rowNum = checkRow[coordinate.split('')[0]]
+      const colNum = coordinate.split('')[1]
+      // row string
+      const row = puzzle.substring((rowNum - 1) * 9, rowNum * 9)
+
+      // column string
+      let colCount = colNum
+      const column = puzzle.split('').filter((d, i) => {
+        if (i % 9 === 0) {
+          colCount = colNum
+        }
+        colCount--
+        if (colCount === 0) {
+          return d
+        }
+      }).join('')
+
+      // region string
+      let rowCount = 0
+      const region = puzzle.split('').filter((d, i) => {
+        if (Math.floor(i / 9) !== rowCount) {
+          rowCount += 1
+        }
+        if (Math.floor(rowCount / 3) === Math.floor(rowNum / 3)) {
+          if (Math.floor((i % 9) / 3) === Math.floor(colNum / 3)) {
+            return d
+          }
+        }
+      }).join('')
+
+      // check row placement
+      const rowResult = solver.checkRowPlacement(puzzle, row, column, value)
+      // check column placement
+      const columnResult = solver.checkColPlacement(puzzle, row, column, value)
+
+      // check region placement
+      const regionResult = solver.checkRegionPlacement(puzzleString, row, column, region, value)
+
+      if (regionResult === false || columnResult === false || rowResult === false) {
+        let conflict = []
+        if (!regionResult) {
+          conflict.push("region")
+        }
+        if (!columnResult) {
+          conflict.push("column")
+        }
+        if (!rowResult) {
+          conflict.push("row")
+        }
+        console.log({ valid: false, conflict: conflict })
+        res.send({ valid: false, conflict: conflict })
+      }
+      console.log({ valid: true })
+      res.send({ valid: true })
     });
 
   app.route('/api/solve')

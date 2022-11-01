4d33270b0c9abc434726d8806b4a2ad72da0fd65
neeraj <neerajp4321@gmail.com>
2022-11-01 11:09:45 +0530

    Feat: add unit tests

--- boilerplate-project-sudoku-solver/tests/1_unit-tests.js
+++ boilerplate-project-sudoku-solver/tests/1_unit-tests.js
@@ -8,6 +8,8 @@ let solver = new Solver();
 suite('Unit Tests', () => {
   const validPuzzleString = '5..91372.3...8.5.9.9.25..8.68.47.23...95..46.7.4.....5.2.......4..8916..85.72...3'
 
+  const validPuzzleAns = '568913724342687519197254386685479231219538467734162895926345178473891652851726943'
+
   const invalidPuzzleString = '5..91372.3...8.5.9.9.25..8.68.47.23...95..46.7.4.....5.2.......4..8916..85.72...a'
 
   test('#1 handle a valid puzzle string', function () {
@@ -16,6 +18,46 @@ suite('Unit Tests', () => {
 
   test('#2 handle puzzle with invalid characters (not 1-9 or .)', function () {
     assert.equal(solver.validate(invalidPuzzleString), 'Invalid characters in puzzle')
+  })
+
+  test('#3 a puzzle string that is not 81 characters in length', function () {
+    assert.equal(solver.validate(validPuzzleString + '3'), 'Expected puzzle to be 81 characters long')
+  })
+
+  test('#4 a valid row placement', function () {
+    assert.equal(solver.checkRowPlacement(validPuzzleString, 1, 2, 6), true)
+  })
+
+  test('#5 an invalid row placement', function () {
+    assert.equal(solver.checkRowPlacement(validPuzzleString, 1, 2, 5), false)
+  })
+
+  test('#6 valid column placement', function () {
+    assert.equal(solver.checkColPlacement(validPuzzleString, 3, 1, 1), true)
+  })
+
+  test('#7 invalid column placement', function () {
+    assert.equal(solver.checkColPlacement(validPuzzleString, 3, 1, 5), false)
+  })
+
+  test('#8 valid region placement', function () {
+    assert.equal(solver.checkRegionPlacement(validPuzzleString, 1, 1, 5), true)
+  })
+
+  test('#9 invalid region placement', function () {
+    assert.equal(solver.checkRegionPlacement(validPuzzleString, 1, 1, 3), false)
+  })
+
+  test('#10 valid string pass the solver', function () {
+    assert.equal(solver.solve(validPuzzleString), validPuzzleAns)
+  })
+
+  test('#11 invalid puzzle string fail the solver', function () {
+    assert.equal(solver.solve(invalidPuzzleString), false)
+  })
+
+  test('#12 incomplete puzzle solve', function () {
+    assert.equal(solver.solve(invalidPuzzleString), validPuzzleAns)
   })
 
 });

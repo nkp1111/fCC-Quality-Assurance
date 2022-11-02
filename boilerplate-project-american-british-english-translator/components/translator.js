bd23551ae9f0a2a68ab93ba322aa5735465bd528
neeraj <neerajp4321@gmail.com>
2022-11-02 18:20:56 +0530

    Feat: add logic to translate american to british

--- boilerplate-project-american-british-english-translator/components/translator.js
+++ boilerplate-project-american-british-english-translator/components/translator.js
@@ -4,6 +4,36 @@ const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
 class Translator {
+  translate(text, locale) {
+    const textParts = text.split(' ')
+    if (locale === 'british-to-american') {
+      return this.BritishToAmerican(textParts).join(' ')
+    }
+
+    if (locale === 'american-to-british') {
+      return this.AmericanToBritish(textParts).join(' ')
+    }
+  }
+
+  AmericanToBritish(textArr) {
+    for (let i = 0; i < textArr.length; i++) {
+      if (americanToBritishSpelling[textArr[i]]) {
+        textArr[i] = `<span class="highlight">${americanToBritishSpelling[textArr[i]]}</span>`
+      }
+      // for time 
+
+    }
+    return textArr
+  }
+
+  BritishToAmerican(textArr) {
+    for (let i = 0; i < textArr.length; i++) {
+      if (americanToBritishSpelling[textArr[i]]) {
+        textArr[i] = `<span class="highlight">${americanToBritishSpelling[textArr[i]]}</span>`
+      }
+    }
+    return textArr
+  }
 
 }
 

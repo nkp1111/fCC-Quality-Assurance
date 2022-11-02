dffc6f7a62643b5b47f786e8dc15f86d4ed249e3
neeraj <neerajp4321@gmail.com>
2022-11-02 19:12:10 +0530

    Feat: add logic to get american translation

--- boilerplate-project-american-british-english-translator/components/translator.js
+++ boilerplate-project-american-british-english-translator/components/translator.js
@@ -4,6 +4,7 @@ const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
 class Translator {
+
   translate(text, locale) {
     const textParts = text.split(' ')
     if (locale === 'british-to-american') {
@@ -31,7 +32,7 @@ class Translator {
         textArr[i] = this.highlightTranslated(translDate)
       }
       // for title
-      if (americanToBritishTitles[textArr[i].toLowerCase()]) {
+      if (transTitle) {
         textArr[i] = this.highlightTranslated(transTitle[0].toUpperCase() + transTitle.substring(1))
       }
     }
@@ -40,16 +41,37 @@ class Translator {
   }
 
   BritishToAmerican(textArr) {
+
     for (let i = 0; i < textArr.length; i++) {
-      if (americanToBritishSpelling[textArr[i]]) {
-        textArr[i] = `<span class="highlight">${americanToBritishSpelling[textArr[i]]}</span>`
+
+      let translSpell = this.getBritishToAmerican(americanToBritishSpelling, textArr[i])
+      let transTitle = this.getBritishToAmerican(americanToBritishTitles, textArr[i].toLowerCase())
+
+      if (translSpell) {
+        textArr[i] = this.highlightTranslated(translSpell)
+      }
+      // for time 
+      if (/^[0-9]+\.[0-9]+$/.test(textArr[i])) {
+        console.log(textArr[i])
+        let translDate = textArr[i].split(':').join('.')
+        console.log(textArr[i])
+        textArr[i] = this.highlightTranslated(translDate)
+      }
+      // for title
+      if (transTitle) {
+        textArr[i] = this.highlightTranslated(transTitle[0].toUpperCase() + transTitle.substring(1))
       }
     }
+
     return textArr
   }
 
   highlightTranslated(text) {
     return `<span class="highlight">${text}</span>`
+  }
+
+  getBritishToAmerican(object, value) {
+    return Object.keys(object).find(key => object[key] === value)
   }
 
 }

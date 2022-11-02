6d16fc67a721f683fb3888dea6c42276edb742c9
neeraj <neerajp4321@gmail.com>
2022-11-02 17:40:13 +0530

    Feat: add checks for empty text and locale

--- boilerplate-project-american-british-english-translator/routes/api.js
+++ boilerplate-project-american-british-english-translator/routes/api.js
@@ -3,11 +3,28 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
+
   const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const { text, locale } = req.body
+
+      // if text is empty
+      if (!text) {
+        res.send({ error: 'No text to translate' })
+      }
+      // if any field is missing
+      if (!text || !locale) {
+        res.send({ error: 'Required field(s) missing' })
+      }
+      // if locale is invalid
+      const possibleLocales = ['british-to-american', 'american-to-british']
+      if (!possibleLocales.includes(locale)) {
+        res.send({ error: 'Invalid value for locale field' })
+      }
+
+      let translation = translator.translate(text, locale)
+      res.send({ text, translation })
     });
 };
